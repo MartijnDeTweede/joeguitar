cd0772be7dff8743a68caf2a740162209f03d40e
Martijn Schotel <schotel.mc@gmail.com>
2020-03-04 20:38:51 +0100

    final changes to content for first teaser lesson

--- src/Containers/HomeContainer.tsx
+++ src/Containers/HomeContainer.tsx
@@ -12,7 +12,7 @@ export const HomeContainer: React.FC = () => {
   <div>
     <Textmedia
       mediaItem={<VideoItem videoId="JQd2y9MWRuE"/>}
-      textItem={<TextItem title="Welcome at joeguitar" texts={[
+      textItem={<TextItem title="Welcome at Joe Guitar" texts={[
         "Joe Guitar is an online platform with guitar lessons for the average Joe and Jane,  with fun material and easy to follow step by step instructions.",
 "You can take the online course as is; in this case you might at some point want to get more into detail with the step by step in depth videos of the lessons at patreon.",
 "You can also enjoy online classes alongside with skype or face to face lessons; in which case you can always ask for personal help when things aren’t clear to you."
@@ -23,11 +23,11 @@ export const HomeContainer: React.FC = () => {
       mediaItem={<ImageItem src={ourFirstLesson} alt="ourFirstLesson" externalLink="https://www.patreon.com/joeguitar"/>}
       textItem={
       <TextItem
-        title="Our very first lesson"
+        title="Introduction to the beginners course"
         texts={[
-          "Are you curious at what we have to offer. You can check out our first lesson on Patreon.",
+          "Are you curious at what we have to offer. You can check out the beginners course at Patreon.",
         ]}
-        linkText="Visit our first lesson"
+        linkText="Visit the beginners course."
         externalLink="https://www.patreon.com/joeguitar"
          />}
       invert
